05eb04cda2a204bf44b9adc4705bbf20cd178d33
amannnkhan <khanaman1764@gmail.com>
2023-03-16 12:08:13 +0530

    program to convert lower case to upper case

---
+++ c2tc/src/com/lib/tns/Lowercase.java
@@ -0,0 +1,11 @@
+package com.lib.tns;
+import java.util.Scanner;
+public class Lowercase {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("program to conert lower case to upper case");
+		String st = sc.nextLine();
+		System.out.println(st.toUpperCase());
+	}
+
+}
