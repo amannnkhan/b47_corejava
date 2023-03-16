8f421102dcaa278a553fad89196a54c9eb3ac8b3
amannnkhan <khanaman1764@gmail.com>
2023-03-16 16:37:22 +0530

    First commit

---
+++ c2tc/src/com/lib/tns/Executor.java
@@ -0,0 +1,16 @@
+package com.lib.tns;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		car obj = new car();
+		obj.forward();
+		obj.backward();
+		obj.stop();
+		System.out.println("the reg no is:"+obj.reg);
+		
+
+	}
+
+}
---
+++ c2tc/src/com/lib/tns/car.java
@@ -0,0 +1,18 @@
+package com.lib.tns;
+
+public class car {
+	int model;//data members
+	String color;
+	int reg = 233;
+	int amount =100000;
+	void forward() {// member functions
+		System.out.println("the car is moving forward");
+	}
+	void backward() {
+		System.out.println("The car is moving backwards");
+		
+	}
+	void stop() {
+		System.out.println("the vehicle is stopped");
+	}
+}
