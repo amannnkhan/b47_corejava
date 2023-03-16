4d7dcc272e5f34fb5c4763aa3f87f4ad106e4ed9
amannnkhan <khanaman1764@gmail.com>
2023-03-16 11:57:22 +0530

    Java program to add two numbers

---
+++ c2tc/src/com/lib/tns/Add.java
@@ -0,0 +1,18 @@
+package com.lib.tns;
+import java.util.Scanner;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the number");
+		int a = sc.nextInt();
+		System.out.println("enter the second number");
+		int b = sc.nextInt();
+	    int sum = a+b;
+		System.out.println("the result\n"+sum);
+
+	}
+
+}
